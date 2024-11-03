dde86284f530d95281059a05c98bbe12294b4e3c
Yuniel Alvarez <yunielap@gmail.com>
2024-11-03 06:46:32 -0500

    Django curse

---
+++ app/app/tests.py
@@ -0,0 +1,12 @@
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
